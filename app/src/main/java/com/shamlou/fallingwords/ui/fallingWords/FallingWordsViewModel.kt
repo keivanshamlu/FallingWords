097a80032f1491d8ca296883388c2254b975651e
keivan <keivan.shamlu.co@gmail.com>
2022-05-08 22:17:56 +0430

    set up counter, view for wrong and correct answers

--- app/src/main/java/com/shamlou/fallingwords/ui/fallingWords/FallingWordsViewModel.kt
+++ app/src/main/java/com/shamlou/fallingwords/ui/fallingWords/FallingWordsViewModel.kt
@@ -10,20 +10,40 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
+const val START_TIME_IN_MILLIS: Long = 50_000
+
 class FallingWordsViewModel(
     private val repo: WordsRepository
-):ViewModel() {
+) : ViewModel() {
 
 
     private val _allWords = MutableStateFlow<Resource<List<ResponseWord>?>>(Resource.success(null))
     val allWords: StateFlow<Resource<List<ResponseWord>?>>
         get() = _allWords
 
+    private val _timeLeft = MutableStateFlow(START_TIME_IN_MILLIS)
+    val timeLeft: StateFlow<Long>
+        get() = _timeLeft
+
+
     init {
 
         getAllWords()
     }
 
+    //set new time left every second
+    fun setTimeLeft(timeLeft: Long){
+
+        _timeLeft.tryEmit(timeLeft)
+    }
+
+    //when game is over
+    fun timerFinished(){
+
+
+    }
+
+    //called at the beginning to get all words
     private fun getAllWords() = viewModelScope.launch {
 
         repo.getWords().collect {
--- app/src/main/java/com/shamlou/fallingwords/ui/fallingWords/FragmentFallingWords.kt
+++ app/src/main/java/com/shamlou/fallingwords/ui/fallingWords/FragmentFallingWords.kt
@@ -1,6 +1,7 @@
 package com.shamlou.fallingwords.ui.fallingWords
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,12 +15,15 @@ import com.shamlou.fallingwords.databinding.FragmentFallingWordsBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
 
-class FragmentFallingWords : Fragment(R.layout.fragment_falling_words){
+
+class FragmentFallingWords : Fragment(R.layout.fragment_falling_words) {
 
     private val viewModel: FallingWordsViewModel by viewModel()
 
     private var _binding: FragmentFallingWordsBinding? = null
+
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
@@ -45,20 +49,57 @@ class FragmentFallingWords : Fragment(R.layout.fragment_falling_words){
         observeViewModel()
     }
 
-    private fun setUpView(){
+    private fun setUpView() {
 
-
+        binding.buttonCorrect.setOnClickListener {
+            startTimer()
+        }
     }
 
-    private fun observeViewModel(){
+    private fun observeViewModel() {
 
+        //gets all words and also have the state of loading, error, success
         lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.allWords.collect {
 
-                    Toast.makeText(requireContext(), "${it.data?.size} - ${it.status.name}", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        requireContext(),
+                        "${it.data?.size} - ${it.status.name}",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
+
+        //observes timeleft and show the last value in UI
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.timeLeft.collect {
+
+                    val minutes = (it / 1000).toInt() / 60
+                    val seconds = (it / 1000).toInt() % 60
+                    val timeLeftFormatted: String = java.lang.String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+                    binding.textViewCountDown.text = timeLeftFormatted
+                }
+            }
+        }
+    }
+
+    //starts the counter with value of [timeLeft] in viewModel
+    //i keep the time in viewmodel so it can survive configure changes
+    private fun startTimer() {
+
+        object : CountDownTimer(viewModel.timeLeft.value, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+
+                viewModel.setTimeLeft(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+
+                viewModel.timerFinished()
+            }
+        }.start()
     }
 }
